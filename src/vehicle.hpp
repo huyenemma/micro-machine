82d9c4c53dabf68850e9d2510f032d8c7a7a868a
Hoang Quan <quan.hoang@aalto.fi>
2023-11-10 00:25:08 +0200

    Replace vehicle.hpp

--- src/vehicle.hpp
+++ src/vehicle.hpp
@@ -8,7 +8,7 @@ class Vehicle
 private:
     bool forceOn=false;
     b2Body* m_body;
-    float maxSpeed=10;
+    float maxSpeed=3;
 public:
     Vehicle(b2World* world, float x,float y);
 
@@ -23,14 +23,13 @@ public:
         m_body->ApplyForceToCenter(force, true);    
     };
 
-    //positive value of torque means the object turn right.
-        void Rotate(float torque=1) {
-        m_body->ApplyAngularImpulse(torque, true);
+    void Rotate(float torque=1) {
+    m_body->ApplyAngularImpulse(torque, true);
     }
 
     std::pair<float, float> GetPosition() {
-        b2Vec2 position = m_body->GetWorldCenter();
-        return std::make_pair(position.x, position.y);
+    b2Vec2 position = m_body->GetWorldCenter();
+    return std::make_pair(position.x, position.y);
     }
 
     void ToggleForce(bool value){
@@ -40,6 +39,10 @@ public:
     float GetAngle(){
         return m_body->GetAngle();
     }
+
+    void ProcessItem(){
+
+    };
 
 
 
@@ -51,21 +54,24 @@ Vehicle::Vehicle(b2World* world, float x=0,float y=0)
     bodyDef.type = b2_dynamicBody;
     bodyDef.position.Set(x,y);
     
-    bodyDef.linearDamping = 0.05f;
+    bodyDef.linearDamping = 0.6f;
     bodyDef.angularDamping = 0.95f;
     bodyDef.awake = true;
     b2PolygonShape dynamicBox;
     dynamicBox.SetAsBox(1.0f, 1.0f);
-
+    
 
     b2FixtureDef fixtureDef;
     fixtureDef.shape = &dynamicBox;
     fixtureDef.density = 1.0f;
     fixtureDef.friction = 0.3f;
+    
 
 
     m_body = world->CreateBody(&bodyDef);
     m_body->CreateFixture(&fixtureDef);
+    b2BodyUserData data = m_body->GetUserData(); 
+    uintptr_t uintptrValue = reinterpret_cast<uintptr_t>(this);
     
 }
 
