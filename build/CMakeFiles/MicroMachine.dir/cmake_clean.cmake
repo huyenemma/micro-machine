7efed4f877e9b501d1f2ed09c732583e6e42b51c
To Minh Duy <tominhduy@Tos-MacBook-Air.local>
2023-11-10 09:42:40 +0200

    libs mod, cmakelist mod

---
+++ build/CMakeFiles/MicroMachine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MicroMachine.dir/src/main.cpp.o"
+  "CMakeFiles/MicroMachine.dir/src/main.cpp.o.d"
+  "MicroMachine"
+  "MicroMachine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MicroMachine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
