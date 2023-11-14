ef98ed46a9d65c0ee98b829afd7d5220c65034b6
Hoang Quan <quan.hoang@aalto.fi>
2023-11-14 18:23:18 +0200

    Upload New File

---
+++ src/buff.hpp
@@ -0,0 +1,22 @@
+
+#ifndef BUFF_HPP
+#define BUFF_HPP
+
+#include <string> 
+#include "vehicle.hpp"
+#include "timer.hpp"
+
+class Buff : public Timer {
+private:
+    std::string id; 
+public:
+    Buff(Vehicle* vehicle, std::string id ,int duration); 
+
+    virtual ~Buff(){};
+
+    virtual void ApplyEffect() = 0;
+
+    virtual void ReverseEffect() = 0;
+};
+
+#endif
