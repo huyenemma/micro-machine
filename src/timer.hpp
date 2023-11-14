aeea8342e24ebe909a1ff57e79914c26d9a3ae65
Hoang Quan <quan.hoang@aalto.fi>
2023-11-14 18:23:41 +0200

    Upload New File

---
+++ src/timer.hpp
@@ -0,0 +1,26 @@
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <string>
+
+class Timer {
+private:
+    std::string id;
+    int timeLeft;
+
+public:
+    Timer(std::string id, int timeLeft);
+
+    virtual ~Timer();
+
+    // Return true if the object should be removed, false otherwise
+    bool Tick() {
+        if (timeLeft > 0) {
+            timeLeft--;
+        }
+        // Return true if timeLeft is zero, indicating that the object should be removed
+        return (timeLeft == 0);
+    }
+};
+
+#endif
