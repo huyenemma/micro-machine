c2cec254255e8bbe1ab42df81a987c7ac7fc32e5
quan <hohoeinear@gmail.com>
2023-11-15 21:52:06 +0200

    Add some skeleton file for buff

--- src/negativeBuff.cpp
+++ src/negativeBuff.cpp
@@ -10,7 +10,6 @@ void ReverseMushroom::ReverseEffect(){
     vehicle->ApplyBuff(-1.0f/buffIntensity,1/buffIntensity,1/buffIntensity,-1.0f/buffIntensity);
 }
 
-
 ReverseMushroom::ReverseMushroom(Vehicle* vehicle,std::string id,int duration,float Intensity):Buff(vehicle,id,duration) {
     buffIntensity = Intensity;
 }
