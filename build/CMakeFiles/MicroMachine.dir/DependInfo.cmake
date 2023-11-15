4ffdc0e652572967d421207f4881d8aef3f0068f
huyendo <huyen.do@aalto.fi>
2023-11-15 22:16:21 +0200

    add img

--- build/CMakeFiles/MicroMachine.dir/DependInfo.cmake
+++ build/CMakeFiles/MicroMachine.dir/DependInfo.cmake
@@ -8,19 +8,22 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/linh/micro-machines-tuukka-himanka-1/src/ContactListener.cpp" "CMakeFiles/MicroMachine.dir/src/ContactListener.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/ContactListener.cpp.o.d"
-  "/Users/linh/micro-machines-tuukka-himanka-1/src/colletable.cpp" "CMakeFiles/MicroMachine.dir/src/colletable.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/colletable.cpp.o.d"
-  "/Users/linh/micro-machines-tuukka-himanka-1/src/game.cpp" "CMakeFiles/MicroMachine.dir/src/game.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/game.cpp.o.d"
-  "/Users/linh/micro-machines-tuukka-himanka-1/src/main.cpp" "CMakeFiles/MicroMachine.dir/src/main.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/main.cpp.o.d"
-  "/Users/linh/micro-machines-tuukka-himanka-1/src/map.cpp" "CMakeFiles/MicroMachine.dir/src/map.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/map.cpp.o.d"
-  "/Users/linh/micro-machines-tuukka-himanka-1/src/reScale.cpp" "CMakeFiles/MicroMachine.dir/src/reScale.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/reScale.cpp.o.d"
-  "/Users/linh/micro-machines-tuukka-himanka-1/src/userDataPointer.cpp" "CMakeFiles/MicroMachine.dir/src/userDataPointer.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/userDataPointer.cpp.o.d"
-  "/Users/linh/micro-machines-tuukka-himanka-1/src/vehicle.cpp" "CMakeFiles/MicroMachine.dir/src/vehicle.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/vehicle.cpp.o.d"
-  "/Users/linh/micro-machines-tuukka-himanka-1/src/world.cpp" "CMakeFiles/MicroMachine.dir/src/world.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/world.cpp.o.d"
+  "/home/huyenemma/cpp/micro/src/ContactListener.cpp" "CMakeFiles/MicroMachine.dir/src/ContactListener.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/ContactListener.cpp.o.d"
+  "/home/huyenemma/cpp/micro/src/collectableBad.cpp" "CMakeFiles/MicroMachine.dir/src/collectableBad.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/collectableBad.cpp.o.d"
+  "/home/huyenemma/cpp/micro/src/collectableSuper.cpp" "CMakeFiles/MicroMachine.dir/src/collectableSuper.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/collectableSuper.cpp.o.d"
+  "/home/huyenemma/cpp/micro/src/colletable.cpp" "CMakeFiles/MicroMachine.dir/src/colletable.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/colletable.cpp.o.d"
+  "/home/huyenemma/cpp/micro/src/colletableGood.cpp" "CMakeFiles/MicroMachine.dir/src/colletableGood.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/colletableGood.cpp.o.d"
+  "/home/huyenemma/cpp/micro/src/game.cpp" "CMakeFiles/MicroMachine.dir/src/game.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/game.cpp.o.d"
+  "/home/huyenemma/cpp/micro/src/main.cpp" "CMakeFiles/MicroMachine.dir/src/main.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/main.cpp.o.d"
+  "/home/huyenemma/cpp/micro/src/map.cpp" "CMakeFiles/MicroMachine.dir/src/map.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/map.cpp.o.d"
+  "/home/huyenemma/cpp/micro/src/reScale.cpp" "CMakeFiles/MicroMachine.dir/src/reScale.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/reScale.cpp.o.d"
+  "/home/huyenemma/cpp/micro/src/userDataPointer.cpp" "CMakeFiles/MicroMachine.dir/src/userDataPointer.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/userDataPointer.cpp.o.d"
+  "/home/huyenemma/cpp/micro/src/vehicle.cpp" "CMakeFiles/MicroMachine.dir/src/vehicle.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/vehicle.cpp.o.d"
+  "/home/huyenemma/cpp/micro/src/world.cpp" "CMakeFiles/MicroMachine.dir/src/world.cpp.o" "gcc" "CMakeFiles/MicroMachine.dir/src/world.cpp.o.d"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
