249c7b622c785213c4b021339e8c4c465e48b9c4
quan <hohoeinear@gmail.com>
2023-11-28 15:26:00 +0200

    add two more animal to the game:cat and horse

--- src/positiveBuff.cpp
+++ src/positiveBuff.cpp
@@ -3,16 +3,3 @@
 #include "vehicle.hpp"
 using namespace PositiveBuff;
 
-void OxSuperSkillBuff::ApplyEffect(Vehicle* vehicle){
-    vehicle->ApplyBuff(buffIntensity,buffIntensity,1,0.001);
-}
-
-void OxSuperSkillBuff::ReverseEffect(Vehicle* vehicle){
-    vehicle->ApplyBuff(1.0f/buffIntensity,1/buffIntensity,1,1.0f/0.001);
-}
-
-OxSuperSkillBuff::OxSuperSkillBuff(int duration,float Intensity) : Buff(duration,BuffType::Positive,BuffEffect::continuous) {
-    buffIntensity = Intensity;
-}
-
-OxSuperSkillBuff::~OxSuperSkillBuff() {}
