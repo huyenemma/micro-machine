b71f5054cd56e7072f2e60fb104912ed0ccf492f
ponyo19 <tominhduy09@gmail.com>
2023-12-03 21:47:26 +0200

    fix bugs

--- src/include/ContactListener.hpp
+++ src/include/ContactListener.hpp
@@ -1,8 +1,11 @@
 #ifndef MY_CONTACT_LISTENER_HPP
 #define MY_CONTACT_LISTENER_HPP
 
-#include <Box2D/Box2D.h>
-#include <vector>
+#include <iostream>
+
+#include "box2dInclude.hpp"
+#include "collectable.hpp"
+#include "obstacle.hpp"
 #include "userDataPointer.hpp"
 
 /**
@@ -10,47 +13,49 @@
  * @brief A custom contact listener class for handling Box2D contact events.
  */
 class MyContactListener : public b2ContactListener {
-public:
-    /**
-     * @brief Default constructor for MyContactListener.
-     */
-    MyContactListener();
+ public:
+  /**
+   * @brief Default constructor for MyContactListener.
+   */
+  MyContactListener();
 
-    /**
-     * @brief Called when two fixtures start to touch.
-     * @param contact The contact object containing information about the contact.
-     */
-    void BeginContact(b2Contact* contact) override;
+  /**
+   * @brief Called when two fixtures start to touch.
+   * @param contact The contact object containing information about the contact.
+   */
+  void BeginContact(b2Contact* contact) override;
 
-    /**
-     * @brief Called when two fixtures cease to touch.
-     * @param contact The contact object containing information about the contact.
-     */
-    void EndContact(b2Contact* contact) override;
+  /**
+   * @brief Called when two fixtures cease to touch.
+   * @param contact The contact object containing information about the contact.
+   */
+  void EndContact(b2Contact* contact) override;
 
-    /**
-     * @brief Destructor for MyContactListener.
-     */
-    ~MyContactListener();
+  /**
+   * @brief Destructor for MyContactListener.
+   */
+  ~MyContactListener();
 
-private:
-    /**
-     * @struct ContactData
-     * @brief Structure containing information about the contacting bodies.
-     */
-    struct ContactData {
-        b2Body* bodyA; ///< Pointer to the first body in contact.
-        b2Body* bodyB; ///< Pointer to the second body in contact.
-    };
+ private:
+  /**
+   * @struct ContactData
+   * @brief Structure containing information about the contacting bodies.
+   */
+  struct ContactData {
+    b2Body* bodyA;  ///< Pointer to the first body in contact.
+    b2Body* bodyB;  ///< Pointer to the second body in contact.
+  };
 
-    std::vector<ContactData> contactsToDelete; ///< List of contacts to be deleted.
+  std::vector<ContactData>
+      contactsToDelete;  ///< List of contacts to be deleted.
 
-    /**
-     * @brief Handles the contact event.
-     * @param contact The contact object containing information about the contact.
-     * @param begin True if it's the beginning of the contact, false if it's the end.
-     */
-    void HandleContact(b2Contact* contact, bool begin);
+  /**
+   * @brief Handles the contact event.
+   * @param contact The contact object containing information about the contact.
+   * @param begin True if it's the beginning of the contact, false if it's the
+   * end.
+   */
+  void HandleContact(b2Contact* contact, bool begin);
 };
 
-#endif // MY_CONTACT_LISTENER_HPP
+#endif  // MY_CONTACT_LISTENER_HPP
--- src/include/timer.hpp
+++ src/include/timer.hpp
@@ -1,35 +1,34 @@
 #ifndef TIMER_HPP
 #define TIMER_HPP
 
-#include <string>
 #include <iostream>
+#include <string>
 
 /**
  * @class Timer
  * @brief Represents a simple countdown timer.
  */
 class Timer {
-private:
-    int timeLeft; ///< The time remaining on the timer.
+ private:
+  int timeLeft;  ///< The time remaining on the timer.
 
-public:
-    /**
-     * @brief Constructor for Timer.
-     * @param timeLeft The initial time left on the timer.
-     */
-    Timer(int timeLeft);
+ public:
+  /**
+   * @brief Constructor for Timer.
+   * @param timeLeft The initial time left on the timer.
+   */
+  Timer(int timeLeft);
 
-    /**
-     * @brief Destructor for Timer.
-     */
-    virtual ~Timer();
+  /**
+   * @brief Destructor for Timer.
+   */
+  virtual ~Timer(){};
 
-    /**
-     * @brief Decreases the time remaining on the timer by one tick.
-     * @return True if the object should be removed, false otherwise.
-     */
-    bool Tick();
-
+  /**
+   * @brief Decreases the time remaining on the timer by one tick.
+   * @return True if the object should be removed, false otherwise.
+   */
+  bool Tick();
 };
 
-#endif // TIMER_HPP
+#endif  // TIMER_HPP
