66af96b23b0634caf075a159742eb1e880ff5a78
ponyo19 <tominhduy09@gmail.com>
2023-12-10 01:30:49 +0200

    add testing report

--- tests/timerTest.cpp
+++ tests/timerTest.cpp
@@ -5,12 +5,18 @@
 // Test fixture for Timer class
 struct Tests : public ::testing::Test {
   Timer* timer;
-  virtual void SetUp() override { timer = new Timer(2); }
+  virtual void SetUp() override {
+    // Set up a timer instance
+    timer = new Timer(2);
+  }
 
+  // Tear down the test fixture
   virtual void TearDown() override { delete timer; }
 };
 
+// Test Tick() function
 TEST_F(Tests, timerTest) {
+  // Check if after 2 ticks the timer should return true
   EXPECT_FALSE(timer->Tick());
   EXPECT_TRUE(timer->Tick());
 }
