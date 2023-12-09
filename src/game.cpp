36a0607fb1e22871b479cc3a43247c9d04a160c9
ponyo19 <tominhduy09@gmail.com>
2023-12-10 01:56:57 +0200

    add sound

--- src/game.cpp
+++ src/game.cpp
@@ -1,19 +1,18 @@
 #include "./include/game.hpp"
 
 Game::Game()
-    : window_(sf::VideoMode(800, 800), "Micro machine"), 
-    isRunning_(false),
-    currentState_(GameState::MENU),
-    menu_(window_) {
-
+    : window_(sf::VideoMode(800, 800), "Micro machine"),
+      isRunning_(false),
+      currentState_(GameState::MENU),
+      menu_(window_) {
   world_ = new World(b2Vec2(0.0f, 0.0f));
-  resourceManager_ = new ResourceManager(); 
+  resourceManager_ = new ResourceManager();
 };
 
 Game::~Game() {
   delete world_;
   delete resourceManager_;
-  delete counterClock_;  
+  delete counterClock_;
   delete map_;
 }
 
@@ -22,29 +21,28 @@ using namespace PositiveBuff;
 void Game::Initialize() {
   resourceManager_->LoadFromJson("../src/resources.json");
 
-  const sf::Font& font = resourceManager_->GetFont("clockFont"); 
+  const sf::Font& font = resourceManager_->GetFont("clockFont");
   counterClock_ = new RealTime(15, font);
   counterClock_->SetUp();
 
   const sf::Texture& map_Texture = resourceManager_->GetImage("forest");
-  map_ = new Map(map_Texture); 
+  map_ = new Map(map_Texture);
 
   const sf::Texture& oxTexture = resourceManager_->GetImage("buffalo");
 
-  //add background sound
-  //backgroundBuffer = resourceManager_->GetSoundBackground("grass");
+  // add background sound
+  backgroundBuffer = resourceManager_->GetSoundBackground("grass");
 
-  //background.setBuffer(backgroundBuffer);
-  //background.setLoop(true);
-  //background.setVolume(50);
-  //background.play();
+  background.setBuffer(backgroundBuffer);
+  background.setLoop(true);
+  background.setVolume(50);
+  background.play();
 
-  //Set sound effect
-  //stepBuffer = resourceManager_->GetSoundStep("grass");
+  // Set sound effect
+  stepBuffer = resourceManager_->GetSoundStep("grass");
 
-  //step.setBuffer(stepBuffer);
-  //step.setVolume(40);
-
+  step.setBuffer(stepBuffer);
+  step.setVolume(40);
 
   Ox* ox = new Ox(world_->GetPhysicWorld(), 136.0f / SCALE, 120.0f / SCALE,
                   oxTexture);
@@ -54,29 +52,37 @@ void Game::Initialize() {
 
   if (playerCount == 2) {
     const sf::Texture& goatTexture = resourceManager_->GetImage("goat");
-    Ox* ox2 = new Ox(world_->GetPhysicWorld(), 200.0f / SCALE, 120.0f / SCALE, goatTexture);
+    Ox* ox2 = new Ox(world_->GetPhysicWorld(), 200.0f / SCALE, 120.0f / SCALE,
+                     goatTexture);
     world_->AddVehicle(ox2);
     player2 = ox2;
   }
-  //Setting Contact Listener
+  // Setting Contact Listener
   MyContactListener* contactListener = new MyContactListener();
   world_->GetPhysicWorld()->SetContactListener(contactListener);
 
-  
-  //Setting collectable and buff
+  // Setting collectable and buff
   CrazyRotate* badBuff1 = new CrazyRotate(2, 40.f, 30.f);
 
   const sf::Texture& badTexture1 = resourceManager_->GetImage("badApple");
 
-  Collectable* collectable = new Collectable(world_->GetPhysicWorld(), b2Vec2(440.0f / SCALE, 440.0f / SCALE), 30.0f/SCALE, badBuff1, badTexture1);
+  Collectable* collectable = new Collectable(
+      world_->GetPhysicWorld(), b2Vec2(440.0f / SCALE, 440.0f / SCALE),
+      30.0f / SCALE, badBuff1, badTexture1);
 
   MaxSpeed* buff2 = new MaxSpeed(8, 1.5f);
-  const sf::Texture& collectable2_Texture = resourceManager_->GetImage("goodApple");
-  Collectable* collectable2 = new Collectable(world_->GetPhysicWorld(), b2Vec2(320.0f / SCALE, 320.0f / SCALE), 30.0f/SCALE, buff2, collectable2_Texture);
+  const sf::Texture& collectable2_Texture =
+      resourceManager_->GetImage("goodApple");
+  Collectable* collectable2 = new Collectable(
+      world_->GetPhysicWorld(), b2Vec2(320.0f / SCALE, 320.0f / SCALE),
+      30.0f / SCALE, buff2, collectable2_Texture);
 
   Magnetic* buff3 = new Magnetic(6, 20.f);
-  const sf::Texture& collectable3_Texture = resourceManager_->GetImage("goodBanana");
-  Collectable* collectable3 = new Collectable(world_->GetPhysicWorld(), b2Vec2(200.0f / SCALE, 240.0f / SCALE),50.0f/SCALE, buff3, collectable3_Texture);
+  const sf::Texture& collectable3_Texture =
+      resourceManager_->GetImage("goodBanana");
+  Collectable* collectable3 = new Collectable(
+      world_->GetPhysicWorld(), b2Vec2(200.0f / SCALE, 240.0f / SCALE),
+      50.0f / SCALE, buff3, collectable3_Texture);
 
   world_->AddCollectable(collectable);
   world_->AddCollectable(collectable2);
@@ -88,10 +94,16 @@ void Game::Initialize() {
                                   50.0f / SCALE, "../img/rock.png");
   world_->AddObstacle(obstacle);
   */
-  
-  StartLine* startLine = new StartLine(world_->GetPhysicWorld(),b2Vec2(136.0f/SCALE,120.0f/SCALE),5.0f,5.0f);
-  CheckPoint* checkPoint1 = new CheckPoint(world_->GetPhysicWorld(),b2Vec2(636.0f/SCALE,620.0f/SCALE),5.0f,5.0f);
-  CheckPoint* checkPoint2 = new CheckPoint(world_->GetPhysicWorld(),b2Vec2(136.0f/SCALE,620.0f/SCALE),5.0f,5.0f);
+
+  StartLine* startLine =
+      new StartLine(world_->GetPhysicWorld(),
+                    b2Vec2(136.0f / SCALE, 120.0f / SCALE), 5.0f, 5.0f);
+  CheckPoint* checkPoint1 =
+      new CheckPoint(world_->GetPhysicWorld(),
+                     b2Vec2(636.0f / SCALE, 620.0f / SCALE), 5.0f, 5.0f);
+  CheckPoint* checkPoint2 =
+      new CheckPoint(world_->GetPhysicWorld(),
+                     b2Vec2(136.0f / SCALE, 620.0f / SCALE), 5.0f, 5.0f);
   startLine->AddCheckPoint(checkPoint1);
   startLine->AddCheckPoint(checkPoint2);
   world_->SetRacingTrack(startLine);
@@ -100,27 +112,27 @@ void Game::Initialize() {
 }
 
 bool Game::Run() {
-  isRunning_ = true; 
+  isRunning_ = true;
   sf::Clock clock;
-  
+
   const sf::Time targetFrameTime = sf::seconds(1.0f / 24.0f);
 
-  while (window_.isOpen() && isRunning_ ) {
+  while (window_.isOpen() && isRunning_) {
     sf::Time deltaTime = clock.restart();
-    if (currentState_ == GameState::MENU ) {
-      HandleMenuInput(); 
-      RenderMenu(); 
-    } 
-    else if (currentState_ == GameState::PLAYING && !counterClock_->IsTimeUp()) {
+    if (currentState_ == GameState::MENU) {
+      HandleMenuInput();
+      RenderMenu();
+    } else if (currentState_ == GameState::PLAYING &&
+               !counterClock_->IsTimeUp()) {
       ProcessEvents();
       Update(deltaTime);
       RenderGame();
-    } 
+    }
     sf::Time elapsedTime = clock.getElapsedTime();
-      
-      if (elapsedTime < targetFrameTime) {
-          sf::sleep(targetFrameTime - elapsedTime);
-      }
+
+    if (elapsedTime < targetFrameTime) {
+      sf::sleep(targetFrameTime - elapsedTime);
+    }
   }
   return false;
 }
@@ -130,14 +142,14 @@ void Game::ProcessEvents() {
   while (window_.pollEvent(event)) {
     if (event.type == sf::Event::Closed) {
       window_.close();
-      }
+    }
   }
 }
 
 void Game::HandleInput() {
   const float angle = 4.0f;
   Vehicle* vehicle = world_->GetVehicle()[0];
-  
+
   vehicle->ToggleForce(false);
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
     // Move car
@@ -150,7 +162,7 @@ void Game::HandleInput() {
     vehicle->Rotate(-angle);
   } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) {
     vehicle->SuperSkill();
-  } 
+  }
 
   vehicle->Update();
 
@@ -168,7 +180,7 @@ void Game::HandleInput() {
       vehicle2->Rotate(-angle);
     } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::E)) {
       vehicle2->SuperSkill();
-    } 
+    }
     vehicle2->Update();
   }
 }
@@ -177,27 +189,28 @@ void Game::Update(sf::Time deltaTime) {
   // Update car position
   HandleInput();
   world_->Update(deltaTime.asSeconds(), velocityIterations, positionIterations);
-  counterClock_->Update(); 
+  counterClock_->Update();
   if (world_->HaveAnyOneWin()) {
-    //event
+    // event
   }
 }
 
 void Game::RenderGame() {
-  
   window_.clear();
   sf::Vector2f mapSize(800.0f, 800.0f);
   // Define the view
-  
+
   if (playerCount == 1) {
     sf::View view;
 
-    //view.setCenter(sf::Vector2f(player1->GetPosition().first * SCALE, player1->GetPosition().second * SCALE));
+    // view.setCenter(sf::Vector2f(player1->GetPosition().first * SCALE,
+    // player1->GetPosition().second * SCALE));
     view.zoom(zoomCoef);
 
-    sf::Vector2f clampedCenter = ClampViewCenter(sf::Vector2f(player1->GetPosition().first * SCALE, player1->GetPosition().second * SCALE), 
-                                                              view.getSize(), 
-                                                              mapSize);
+    sf::Vector2f clampedCenter =
+        ClampViewCenter(sf::Vector2f(player1->GetPosition().first * SCALE,
+                                     player1->GetPosition().second * SCALE),
+                        view.getSize(), mapSize);
     view.setCenter(clampedCenter);
     window_.setView(view);
     DrawGameWorld();
@@ -206,55 +219,58 @@ void Game::RenderGame() {
   else if (playerCount == 2) {
     sf::View view1;
     // view1.setCenter(sf::Vector2f(player1->GetPosition().first * SCALE,
-    //                              player1->GetPosition().second * SCALE));     
+    //                              player1->GetPosition().second * SCALE));
     view1.zoom(zoomCoef);
     view1.setViewport(sf::FloatRect(0.f, 0.f, 0.5f, 1.f));
 
-    sf::Vector2f clampedCenter1 = ClampViewCenter(sf::Vector2f(player1->GetPosition().first * SCALE, player1->GetPosition().second * SCALE), 
-                                                              view1.getSize(), 
-                                                              mapSize);
+    sf::Vector2f clampedCenter1 =
+        ClampViewCenter(sf::Vector2f(player1->GetPosition().first * SCALE,
+                                     player1->GetPosition().second * SCALE),
+                        view1.getSize(), mapSize);
     view1.setCenter(clampedCenter1);
     window_.setView(view1);
     DrawGameWorld();
 
     sf::View view2;
     // view2.setCenter(sf::Vector2f(player2->GetPosition().first * SCALE,
-    //                              player2->GetPosition().second * SCALE)); 
+    //                              player2->GetPosition().second * SCALE));
     view2.zoom(zoomCoef);
     view2.setViewport(sf::FloatRect(0.5f, 0.f, 0.5f, 1.f));
 
-    sf::Vector2f clampedCenter2 = ClampViewCenter(sf::Vector2f(player2->GetPosition().first * SCALE, player2->GetPosition().second * SCALE), 
-                                                              view2.getSize(), 
-                                                              mapSize);
+    sf::Vector2f clampedCenter2 =
+        ClampViewCenter(sf::Vector2f(player2->GetPosition().first * SCALE,
+                                     player2->GetPosition().second * SCALE),
+                        view2.getSize(), mapSize);
     view2.setCenter(clampedCenter2);
     window_.setView(view2);
-    
+
     DrawGameWorld();
   }
-    window_.display();
+  window_.display();
 }
 
-//helper function 
-sf::Vector2f Game::ClampViewCenter(const sf::Vector2f& center, const sf::Vector2f& viewSize, const sf::Vector2f& mapSize) {
-    sf::Vector2f clampedCenter = center;
+// helper function
+sf::Vector2f Game::ClampViewCenter(const sf::Vector2f& center,
+                                   const sf::Vector2f& viewSize,
+                                   const sf::Vector2f& mapSize) {
+  sf::Vector2f clampedCenter = center;
 
-    // Clamp X coordinates
-    float minX = viewSize.x / 2;
-    float maxX = mapSize.x - minX;
-    clampedCenter.x = std::max(clampedCenter.x, minX);
-    clampedCenter.x = std::min(clampedCenter.x, maxX);
+  // Clamp X coordinates
+  float minX = viewSize.x / 2;
+  float maxX = mapSize.x - minX;
+  clampedCenter.x = std::max(clampedCenter.x, minX);
+  clampedCenter.x = std::min(clampedCenter.x, maxX);
 
-    // Clamp Y coordinates
-    float minY = viewSize.y / 2;
-    float maxY = mapSize.y - minY;
-    clampedCenter.y = std::max(clampedCenter.y, minY);
-    clampedCenter.y = std::min(clampedCenter.y, maxY);
+  // Clamp Y coordinates
+  float minY = viewSize.y / 2;
+  float maxY = mapSize.y - minY;
+  clampedCenter.y = std::max(clampedCenter.y, minY);
+  clampedCenter.y = std::min(clampedCenter.y, maxY);
 
-    return clampedCenter;
+  return clampedCenter;
 }
 
 void Game::DrawGameWorld() {
-
   map_->Draw(window_);
 
   if (!world_->GetVehicle().empty()) {
@@ -296,26 +312,25 @@ void Game::HandleMenuInput() {
           case sf::Keyboard::Enter:
             int selectedItem = menu_.GetPressedItem();
             if (selectedItem == GameMenu::ONE_PLAYER) {
-                playerCount = 1;
-                currentState_ = GameState::PLAYING;
-                Initialize(); 
+              playerCount = 1;
+              currentState_ = GameState::PLAYING;
+              Initialize();
             } else if (selectedItem == GameMenu::TWO_PLAYER) {
-                playerCount = 2;
-                currentState_ = GameState::PLAYING;
-                Initialize(); 
+              playerCount = 2;
+              currentState_ = GameState::PLAYING;
+              Initialize();
             } else if (selectedItem == GameMenu::EXIT) {
-                window_.close();
+              window_.close();
             }
             break;
         }
         break;
       case sf::Event::Closed:
-          window_.close();
-          break;
+        window_.close();
+        break;
     }
   }
 }
-
 
 void Game::RenderMenu() {
   window_.clear();
@@ -323,11 +338,10 @@ void Game::RenderMenu() {
   window_.display();
 }
 
-
 void Game::AddBoundaries() {
   float world_Width = 800.0f / SCALE;   // Width of window_ in Box2D units
   float world_Height = 800.0f / SCALE;  // Height of  window_ in Box2D units
-  float thickness = 0.0005f / SCALE;   // Thickness of the boundary walls
+  float thickness = 0.0005f / SCALE;    // Thickness of the boundary walls
 
   // Define the positions and sizes of the boundary walls
   b2Vec2 topWallPos(world_Width / 2, thickness / 2);
